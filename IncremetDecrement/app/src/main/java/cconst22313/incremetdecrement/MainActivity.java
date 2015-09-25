10244bb10f885ed4e7fc3c3e83c8c592ee45134d
Carlos Constantino <A22313@alunos.isel.pt>
2015-09-25 10:34:13 +0100

    first two projects: * HelloAndroid - from wizzard * IncrementDecrement - first sample: one text edit with two buttons.   					   One to increment value, other to decrement value

---
+++ IncremetDecrement/app/src/main/java/cconst22313/incremetdecrement/MainActivity.java
@@ -0,0 +1,66 @@
+package cconst22313.incremetdecrement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button incBtn = (Button)findViewById(R.id.incBtn);
+        Button decBtn = (Button)findViewById(R.id.decBtn);
+
+        incBtn.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View v) {
+                    EditText mEdit = (EditText) findViewById(R.id.editTV);
+                    int value = Integer.parseInt(String.valueOf(mEdit.getText()));
+                    value++;
+                    mEdit.setText(value + "");
+                }
+            }
+        );
+
+        decBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        EditText mEdit = (EditText) findViewById(R.id.editTV);
+                        int value = Integer.parseInt(String.valueOf(mEdit.getText()));
+                        value--;
+                        mEdit.setText(value + "");
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
